b59bd8031aebf1e1e935f5b52d87bd43539dcad0
Alex Zylman <alex.zylman@getclever.com>
2014-07-16 10:06:34 -0700

    pull from latest mozsvc plugins
    Now that our sentry PR is merged in to make it use HTTP instead of UDP

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -55,21 +55,6 @@ function(git_clone url tag)
     add_dependencies(GoPackages ${name})
 endfunction(git_clone)
 
-function(git_clone_wacky url tag)
-    parse_url(${url})
-    externalproject_add(
-        ${name}_wacky
-        GIT_REPOSITORY ${url}
-        GIT_TAG ${tag}
-        SOURCE_DIR "${PROJECT_PATH}/src/${path}"
-        BUILD_COMMAND ""
-        CONFIGURE_COMMAND ""
-        INSTALL_COMMAND ""
-        UPDATE_COMMAND "" # comment out to enable updates
-    )
-    add_dependencies(GoPackages ${name})
-endfunction(git_clone_wacky)
-
 function(hg_clone url tag)
     parse_url(${url})
     externalproject_add(
@@ -161,8 +146,7 @@ endif()
 
 if (INCLUDE_MOZSVC)
     git_clone(https://github.com/getsentry/raven-go 0cc1491d9d27b258a9b4f0238908cb0d51bd6c9b)
-    git_clone_wacky(https://github.com/mozilla-services/heka-mozsvc-plugins 5eb86dea7123aa31d5ceeb1b1c0d2bf005b3a287)
-    add_external_plugin(git https://github.com/Clever/heka-mozsvc-plugins 87166bffa2b36a5588f437357e5ca92d756bc100)
+    add_external_plugin(git https://github.com/mozilla-services/heka-mozsvc-plugins 0381a5d004d5552e1ca82e7c5cd66f606e084ba8)
 endif()
 
 if (INCLUDE_DOCUMENTATION)
